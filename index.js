ad00fc928eea2a761e8bcb96c355b844d851a5db
Victor Lagos <victlagos@gmail.com>
2021-08-15 18:05:48 -0400

    agrego edad a objeto persona

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ console.log("Hola mundo 123!!!");
 const persona = {
     nombre: "Victor",
     apellido: "Lagos",
+    edad: "39",
     nombreCompleto(){
         return this.nombre +' '+this.apellido;
     }
